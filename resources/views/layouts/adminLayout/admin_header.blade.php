4f64454cdc1b244813be32e1856f3d05af5120de
Mohd Asyraf <asyraf.ruslaan@gmail.com>
2019-02-09 00:48:37 +0800

    update for the datatables

--- resources/views/layouts/adminLayout/admin_header.blade.php
+++ resources/views/layouts/adminLayout/admin_header.blade.php
@@ -9,12 +9,12 @@
     <ul class="nav">
         <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome Mohd Asyraf</span><b class="caret"></b></a>
             <ul class="dropdown-menu">
-            <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
-            <li class="divider"></li>
-            <li><a href="{{url('/logout')}}"><i class="icon-key"></i> Log Out</a></li>
+            {{-- <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
+            <li class="divider"></li> --}}
+            <li><a href="{{url('/admin/setting')}}"><i class="icon-key"></i> Change Password</a></li>
             </ul>
         </li>
-        <li class=""><a title="" href="{{url('/admin/setting')}}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
+        {{-- <li class=""><a title="" href="{{url('/admin/setting')}}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li> --}}
         <li class=""><a title="" href="{{url('/logout')}}"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
     </ul>
 </div>
